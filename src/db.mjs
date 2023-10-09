343b9625e08c2dc4b353a75112294b29080fd3ee
dina <timcud6@gmail.com>
2023-10-09 06:52:21 +0300

    added multer

--- src/db.mjs
+++ src/db.mjs
@@ -1,30 +1,29 @@
 import pgPromise from 'pg-promise';
 
 const pgp = pgPromise({});
+const db = pgp('postgres://postgres:timcu1995@localhost:5432/planets');
 
-const db = pgp('postgres://postgres:timcu1995@localhost:5432/planets'); 
+const createTableQuery = `
+  DROP TABLE IF EXISTS planets;
+  CREATE TABLE planets(
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    image TEXT
+  );
+`;
 
-async function createTable() {
-    try {
-      await db.none('DROP TABLE IF EXISTS planets;');
-      await db.none('CREATE TABLE planets(id SERIAL NOT NULL PRIMARY KEY, name TEXT NOT NULL);');
-      console.log('Table planets created');
-    } catch (error) {
-      console.error('Error creating table:', error);
-    }
-  }
-  
-  async function insertPlanets() {
-    try {
-      await db.none('INSERT INTO planets (name) VALUES ($1), ($2);', ['Earth', 'Mars']);
-      console.log('Planets inserted');
-    } catch (error) {
-      console.error('Error inserting planets:', error);
-    }
-  }
-  
-  createTable().then(() => {
-    insertPlanets();
+const insertPlanetsQuery = `
+  INSERT INTO planets (name) VALUES 
+  ('Earth'), 
+  ('Mars');
+`;
+
+db.none(createTableQuery + insertPlanetsQuery)
+  .then(() => {
+    console.log('table planets created');
+  })
+  .catch(error => {
+    console.error('error creating table', error);
   });
 
 export default db;
--- src/server.mjs
+++ src/server.mjs
@@ -1,17 +1,33 @@
 import express from 'express';
 import db from './db.mjs';
+import multer from 'multer';
 
 const app = express();
-const port = 3006;
+const port = 3009;
 
 app.use(express.json());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now());
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const handleError = (res, error, message) => {
+  console.error(message, error);
+  res.status(500).send('Server Error');
+};
 
 app.get('/planets', (req, res) => {
   db.any('SELECT * FROM planets;').then(data => {
     res.json(data);
-  }).catch(error => {
-    console.error('error', error);
-    res.status(500).send('Server Error');
+  }).catch((error) => {
+    handleError(res, error, 'error planet:');
   });
 });
 
@@ -20,10 +36,28 @@ app.post('/planets', (req, res) => {
   const planetName = req.body.name;
   db.none('INSERT INTO planets (name) VALUES ($1);', planetName).then(() => {
     res.send('added');
-  }).catch(error => {
-    console.error('error planet:', error);
-    res.status(500).send('Server Error');
+  }).catch((error) => {
+    handleError(res, error, 'error planet:');
   });
+});
+
+const multerErrorHandler = (err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    res.status(400).send('errore nel caricamento');
+  } else {
+    next(err);
+  }
+};
+
+app.post('/planets/:id/image', multerErrorHandler, upload.single('planetImage'), async (req, res) => {
+  try {
+    const planetId = req.params.id;
+    const imagePath = req.file.path;
+    await db.none('UPDATE planets SET image=$1 WHERE id=$2;', [imagePath, planetId]);
+    res.send('immagine aggiornata');
+  } catch (error) {
+    handleError(res, error, 'Errore aggiornamento');
+  }
 });
 
 app.listen(port, () => {
